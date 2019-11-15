9eaad19b719b4312716a29d98284362feafd13c2
ronan <g00342393@gmit.ie>
2019-11-15 12:17:04 +0000

    Added Dropwizard dependency and example code

---
+++ src/main/java/ronan_hanley/dist_sys/user_account_service/AccountServiceApp.java
@@ -0,0 +1,27 @@
+package ronan_hanley.dist_sys.user_account_service;
+
+import io.dropwizard.Application;
+import io.dropwizard.Configuration;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AccountServiceApp extends Application<Configuration> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccountServiceApp.class);
+
+    @Override
+    public void initialize(Bootstrap<Configuration> bootstrap) {
+        // init
+    }
+
+    @Override
+    public void run(Configuration config, Environment env) {
+        LOGGER.info("Run");
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        new AccountServiceApp().run(args);
+    }
+}
